a8bb6515ef9440bd3e91d90a7dd32578a5466419
Austin Mac <austinmac@csil-32.cs.ucsb.edu>
2020-01-30 09:28:33 -0800

    factorial

--- factorial.asm
+++ factorial.asm
@@ -12,14 +12,56 @@
 #   cout << "Factorial of " << x << " is:\n" << fn << "\n";
 #
 .data
+	inputPrompt: .asciiz "Enter a number:\n"
+	factorial: .asciiz "Factorial of "
+	is: .asciiz " is:\n"
 
-	# TODO: Write your initializations here
-
-#Text Area (i.e. instructions)
+# Text Area (i.e. instructions)
 .text
-main:
+	main:
+		# Prompt for input
+		li $v0, 4
+		la $a0, inputPrompt
+		syscall
 
-	# TODO: Write your code here
+		# Load user input
+		li $v0, 5
+		syscall
+		move $t0, $v0
 
-exit:
-	# TODO: Write code to properly exit a SPIM simulation
+		# Print factorial
+		li $v0, 4
+		la $a0, factorial
+		syscall
+		li $v0, 1
+		move $a0, $t0
+		syscall
+		li $v0, 4
+		la $a0, is
+		syscall
+
+		move $t1, $t0
+
+		# check if input is 0
+		bne $t1, $zero, myloop
+		addi $t1, $t1, 1
+		j exit
+
+
+	myloop: 
+		addi $t0, $t0, -1
+		beq $t0, $zero, exit
+
+		mult $t1, $t0
+		mflo $t1
+		j myloop
+
+
+		
+
+	exit:
+		li $v0, 1
+		move $a0, $t1
+		syscall
+		li $v0, 10
+		syscall
